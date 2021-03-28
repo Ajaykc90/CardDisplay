43fd911a6f8a2a4392580a96cd3ad9e4e28e7671
Ajay KC <ajaykc90@gmail.com>
2021-03-28 23:30:33 +1100

    Settingup

---
+++ src/CardDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CardDetails() {
+    return (
+        <div className="homepage">
+            <div className="CardSample">
+
+            </div>
+        </div>
+    )
+}
+
+export default CardDetails
